03af3b0ce52b6a669e56fe5d856dd67c4469d92c
Dmitry Kunin <dkunin@avito.ru>
2018-08-31 06:41:55 +0300

    Added listen method

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ module.exports = {
                 }
                 const progress = dat.importFiles({ watch: true });
                 const network = dat.joinNetwork();
-                resolve({ network, progress, key: dat.key.toString('hex') })
+                resolve({ network, progress, key: dat.key.toString('hex') });
+            });
+        });
+    },
+    listen(config) {
+        return new Promise((resolve, reject) => {
+            Dat(config.path, { key: config.key }, function(err, dat) {
+                const network = dat.joinNetwork(function(err) {
+                    if (err) throw err;
+                    if (!dat.network.connected || !dat.network.connecting) {
+                        console.error(
+                            'No users currently online for that key.'
+                        );
+                        process.exit(1);
+                    }
+                });
+                resolve({ network });
             });
         });
     }
--- test.js
+++ test.js
@@ -2,14 +2,24 @@
 
 const datDeamon = require('./');
 
-datDeamon.start({
-    path: '~/Screenshots'
-}).then(({ network, progress, key }) => {
-    console.log(key);
+// datDeamon.start({
+//     path: '~/Screenshots'
+// }).then(({ network, progress, key }) => {
+//     console.log(key);
+//     network.on('connection', function(connection, info) {
+//         console.log(info);
+//     });
+//     progress.on('put', function(src, dest) {
+//         console.log('Importing ', src.name, ' into archive');
+//     });
+// });
+
+
+datDeamon.listen({
+    path: './temp-screenshots',
+    key: '5bd76913bdbdbfb45d7a30d9982733b7435842de129041d3821bc7b9d3098a3c'
+}).then(({ network }) => {
     network.on('connection', function(connection, info) {
         console.log(info);
-    });
-    progress.on('put', function(src, dest) {
-        console.log('Importing ', src.name, ' into archive');
     });
 });
